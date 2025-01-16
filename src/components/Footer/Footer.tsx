40d8743349190465d33924230f3872a0542a1e01
Marcus Widen <hellowiden@gmail.com>
2025-01-16 10:31:22 +0100

    Added a footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
 
 function Footer() {
-  return <div>Footer</div>;
+  return (
+    <footer className="bg-gray-800 text-white py-6">
+      <div className="container mx-auto text-center">
+        {/* Footer Content */}
+        <p className="text-sm">
+          &copy; {new Date().getFullYear()} Your Company. All rights reserved.
+        </p>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
