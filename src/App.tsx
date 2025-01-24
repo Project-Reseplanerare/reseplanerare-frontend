f7e9af36937dda1d072d0ae85a204c722281580c
Tobias Andersson <tobiaserikandersson@gmail.com>
2025-01-24 11:42:58 +0200

    fixed header bug

--- src/App.tsx
+++ src/App.tsx
@@ -9,30 +9,26 @@ import SearchInput from './components/Reseplanerare/SearchInput';
 
 function App() {
   return (
-
     <Router>
       <main className="app-container">
-      <Header />
+        <Header />
         <Routes>
           <Route
             path="/"
             element={
               <section className="grid-container px-6">
-
-           
-                  <Breadcrumbs 
-                    crumbs={[
-                      { label: 'Visist Värmland', link: '/' },
-                      { label: 'Reseplaneraren', link: '/reseplaneraren' },
-                    ]}
-                  />
-                    
-                  <WidgetHeader />
-                  <SearchInput />
-                  <div className="h-60 border border-gray-300 rounded-lg overflow-hidden flex flex-grow">
-                      <Map />
-                  </div>
-                  <ReseWidget />
+                <Breadcrumbs
+                  crumbs={[
+                    { label: 'Visit Värmland', link: '/' },
+                    { label: 'Reseplaneraren', link: '/reseplaneraren' },
+                  ]}
+                />
+                <WidgetHeader />
+                <SearchInput />
+                <div className="h-60 border border-gray-300 rounded-lg overflow-hidden flex flex-grow">
+                  <Map />
+                </div>
+                <ReseWidget />
               </section>
             }
           />
