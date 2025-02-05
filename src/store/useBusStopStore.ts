034bcb94ffccfc4f14b09daea839d5c6ea354e4b
Tobias Andersson <tobiaserikandersson@gmail.com>
2025-02-05 18:56:13 +0200

    some bus routes on map

--- src/store/useBusStopStore.ts
+++ src/store/useBusStopStore.ts
@@ -1,14 +1,19 @@
-import {create} from 'zustand';
-interface busStopStore {
+import { create } from "zustand";
+
+interface BusStopStore {
   fromStopId: string;
   toStopId: string;
+  stopsCoords: { coords: [number, number]; name?: string }[];
+  setStopsCoords: (stops: { coords: [number, number]; name?: string }[]) => void;
   setFromStopId: (id: string) => void;
   setToStopId: (id: string) => void;
 }
 
-export const useBusStopStore = create<busStopStore>((set) => ({
-  fromStopId: '',
-  toStopId: '',
-  setFromStopId: (id: string) => set({ fromStopId: id }),
-  setToStopId: (id: string) => set({ toStopId: id }),
+export const useBusStopStore = create<BusStopStore>((set) => ({
+  fromStopId: "",
+  toStopId: "",
+  stopsCoords: [],
+  setStopsCoords: (stops) => set({ stopsCoords: stops }),
+  setFromStopId: (id) => set({ fromStopId: id }),
+  setToStopId: (id) => set({ toStopId: id }),
 }));
