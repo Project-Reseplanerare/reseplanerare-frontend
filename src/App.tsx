0f9d16020c5f19fd59eb77e5aec5d1bee4c29ca7
Marcus Widen <hellowiden@gmail.com>
2025-02-12 16:30:13 +0100

    styling fixes

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <Route
             path="/"
             element={
-              <section className="w-full p-4 lg:w-[800px] grid gap-4 h-auto">
+              <section className="w-full p-4 lg:w-[800px] grid gap-4 h-auto ">
                 <GeneralInformation />
                 <SearchInput />
                 <TravelPlannerWrapper />
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,12 +3,16 @@ import logo from '../../assets/logotyp-visitvarmland.svg';
 import ThemeSwitch from '../ThemeSwitch/ThemeSwitch';
 
 const Header = () => (
-  <header className="bg-lightDark dark:bg-darkDark text-darkDark dark:text-lightLight w-full">
+  <header className="shadow-lg bg-lightDark dark:bg-darkDark text-darkDark dark:text-lightLight w-full">
     <nav className="grid grid-cols-[auto_1fr] items-center">
       {/* Left: VisitVärmland Logo */}
       <div className="justify-self-start">
         <Link to="/">
-        <img src={logo} alt="Visit Värmland Logo" className="h-9 dark:filter dark:invert dark:hue-rotate-180 " />
+          <img
+            src={logo}
+            alt="Visit Värmland Logo"
+            className="h-9 dark:filter dark:invert dark:hue-rotate-180 "
+          />
         </Link>
       </div>
 
