1a52a8b346b802b7b6bab0bf656e3a0bbb82cde6
Marcus Widen <hellowiden@gmail.com>
2025-02-13 08:57:22 +0100

    styling fixes

--- src/components/GenaralInformation/GeneralInformation.tsx
+++ src/components/GenaralInformation/GeneralInformation.tsx
@@ -6,13 +6,13 @@ const GeneralInformation: React.FC = () => {
   if (!isVisible) return null;
 
   return (
-    <section className="grid w-full p-6 backdrop-blur-md bg-lightDark/90 dark:bg-darkDark/90 text-darkDark dark:text-lightLight rounded shadow-md  border-darkLight dark:border-lightDark">
+    <section className="grid w-full p-6 backdrop-blur-md bg-lightDark/90 dark:bg-darkDark/90 text-darkDark dark:text-lightLight rounded shadow-md border-darkLight dark:border-lightDark">
       {/* Header Section */}
-      <div className="grid grid-cols-[min-content_1fr] gap-4">
+      <div className="grid grid-cols-[auto_1fr] gap-4 items-center">
         {/* Close Button */}
         <button
           onClick={() => setIsVisible(false)}
-          className="w-8 h-8 flex items-center justify-center text-darkLight dark:text-lightDark border border-darkLight dark:border-lightDark rounded-full font-bold transition hover:text-white dark:hover:text-white hover:bg-darkLight dark:hover:bg-lightDark"
+          className="w-8 h-8 grid place-items-center text-darkLight dark:text-lightDark border border-darkLight dark:border-lightDark rounded-full font-bold transition hover:text-white hover:bg-darkLight dark:hover:text-white dark:hover:bg-lightDark"
           aria-label="Close"
         >
           ✕
