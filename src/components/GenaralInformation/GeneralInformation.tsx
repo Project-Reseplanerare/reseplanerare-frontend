4649e8a92f0fbb7312f99cccdfd0874fdacdf1a2
Marcus Widen <hellowiden@gmail.com>
2025-02-19 13:03:47 +0100

    opt code

--- src/components/GenaralInformation/GeneralInformation.tsx
+++ src/components/GenaralInformation/GeneralInformation.tsx
@@ -3,13 +3,9 @@ import { useState } from 'react';
 const GeneralInformation: React.FC = () => {
   const [isVisible, setIsVisible] = useState(true);
 
-  if (!isVisible) return null;
-
-  return (
+  return isVisible ? (
     <section className="grid w-full p-6 backdrop-blur-md bg-lightDark/90 dark:bg-darkDark/90 text-darkDark dark:text-lightLight rounded shadow-md border-darkLight dark:border-lightDark">
-      {/* Header Section */}
       <div className="grid grid-cols-[auto_1fr] gap-4 items-center">
-        {/* Close Button */}
         <button
           onClick={() => setIsVisible(false)}
           className="hover:scale-105 transition-transform w-8 h-8 grid place-items-center text-lightDark rounded-full font-bold dark:hover:text-white bg-blueLight"
@@ -17,21 +13,16 @@ const GeneralInformation: React.FC = () => {
         >
           ✕
         </button>
-
-        {/* Title */}
         <h1 className="text-2xl font-bold">Reseplaneraren</h1>
       </div>
-
       <hr className="h-0.5 bg-blueLight" />
-
-      {/* Description */}
       <p className="text-darkLight dark:text-lightDark">
         Reseplaneraren hjälper dig att enkelt hitta och planera dina resor i
         Värmland. Oavsett om du pendlar dagligen eller besöker regionen, ger vår
         tjänst dig de bästa resealternativen i samarbete med Värmlandstrafik.
       </p>
     </section>
-  );
+  ) : null;
 };
 
 export default GeneralInformation;
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,18 +5,15 @@ import ThemeSwitch from '../ThemeSwitch/ThemeSwitch';
 const Header = () => (
   <header className="shadow-lg backdrop-blur-md bg-lightDark/90 dark:bg-darkDark/90 text-darkDark dark:text-lightLight w-full">
     <nav className="grid grid-cols-[auto_1fr] items-center">
-      {/* Left: VisitVärmland Logo */}
       <div className="justify-self-start">
         <Link to="/">
           <img
             src={logo}
             alt="Visit Värmland Logo"
-            className="h-9 dark:filter dark:invert dark:hue-rotate-180 "
+            className="h-9 dark:filter dark:invert dark:hue-rotate-180"
           />
         </Link>
       </div>
-
-      {/* Right: ThemeSwitch component */}
       <div className="justify-self-end text-darkLight dark:text-lightDark">
         <ThemeSwitch />
       </div>
