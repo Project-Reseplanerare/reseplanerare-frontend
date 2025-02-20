8c92bbe2df97ea1255ab694f754c9bb6f31282e1
Marcus Widen <hellowiden@gmail.com>
2025-02-20 12:50:43 +0100

    opt code

--- src/components/GenaralInformation/GeneralInformation.tsx
+++ src/components/GenaralInformation/GeneralInformation.tsx
@@ -4,18 +4,25 @@ export const GeneralInformation: React.FC = () => {
   const [isVisible, setIsVisible] = useState(true);
 
   return isVisible ? (
-    <section className="grid w-full p-6 backdrop-blur-md bg-lightDark/90 dark:bg-darkDark/90 text-darkDark dark:text-lightLight rounded shadow-md border-darkLight dark:border-lightDark">
-      <div className="grid grid-cols-[auto_1fr] gap-4 items-center">
+    <section
+      role="alert"
+      className="grid w-full p-6 backdrop-blur-md bg-lightDark/90 dark:bg-darkDark/90 
+        text-darkDark dark:text-lightLight rounded shadow-md border-darkLight dark:border-lightDark"
+    >
+      <header className="grid grid-cols-[auto_1fr] gap-4 items-center">
         <button
           onClick={() => setIsVisible(false)}
-          className="hover:scale-105 transition-transform w-8 h-8 grid place-items-center text-lightDark rounded-full font-bold dark:hover:text-white bg-blueLight"
-          aria-label="Close"
+          className="hover:scale-105 transition-transform w-8 h-8 grid place-items-center 
+            text-lightDark rounded-full font-bold dark:hover:text-white bg-blueLight"
+          aria-label="Close information panel"
         >
           ✕
         </button>
         <h1 className="text-2xl font-bold">Reseplaneraren</h1>
-      </div>
+      </header>
+
       <hr className="h-0.5 bg-blueLight" />
+
       <p className="text-darkLight dark:text-lightDark">
         Reseplaneraren hjälper dig att enkelt hitta och planera dina resor i
         Värmland. Oavsett om du pendlar dagligen eller besöker regionen, ger vår
@@ -24,4 +31,3 @@ export const GeneralInformation: React.FC = () => {
     </section>
   ) : null;
 };
-
