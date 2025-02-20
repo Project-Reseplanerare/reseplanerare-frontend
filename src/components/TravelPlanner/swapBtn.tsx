2de1d2615fcec82c1a9e872655ded0bfd8a2ccd1
Marcus Widen <hellowiden@gmail.com>
2025-02-20 19:09:04 +0100

    added animation to SwapBtn

--- src/components/TravelPlanner/swapBtn.tsx
+++ src/components/TravelPlanner/swapBtn.tsx
@@ -1,24 +1,32 @@
+import { motion } from 'framer-motion';
+
 interface SwapBtnProps {
   isDisabled: boolean;
   onClick: () => void;
 }
 
 export const SwapBtn: React.FC<SwapBtnProps> = ({ isDisabled, onClick }) => (
-  <button
-    className={`flex items-center justify-center rounded-full h-10 w-10 border focus:outline-none transition-transform hover:scale-105 ${
+  <motion.button
+    className={`grid place-items-center rounded-full h-10 w-10 border focus:outline-none transition-transform hover:scale-105 ${
       isDisabled ? '' : 'bg-blueLight text-lightLight'
     }`}
     onClick={onClick}
     aria-label="Swap addresses"
     title="Swap addresses"
     disabled={isDisabled}
+    whileTap={!isDisabled ? { rotate: 180, scale: 1 } : {}}
+    transition={{ type: 'spring', stiffness: 300 }}
   >
-    <svg
+    <motion.svg
       xmlns="http://www.w3.org/2000/svg"
       className="w-6 h-6"
       fill="none"
       viewBox="0 0 24 24"
       stroke="currentColor"
+      initial={{ rotate: 0 }}
+      animate={{ rotate: 0 }}
+      whileTap={!isDisabled ? { rotate: 180 } : {}}
+      transition={{ type: 'spring', stiffness: 300 }}
     >
       <path
         strokeLinecap="round"
@@ -26,6 +34,6 @@ export const SwapBtn: React.FC<SwapBtnProps> = ({ isDisabled, onClick }) => (
         strokeWidth={2}
         d="M7 16V4m0 0L3 8m4-4l4 4m6 0v12m0 0l4-4m-4 4l-4-4"
       />
-    </svg>
-  </button>
+    </motion.svg>
+  </motion.button>
 );
