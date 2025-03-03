cafccc17ee0bf91abffeb3e881908ee61c3ddfc4
LinneaSjoholm <linnea.sjoholm@live.se>
2025-03-03 17:48:59 +0100

    added eventIcon

---
+++ src/components/Map/eventIcon.tsx
@@ -0,0 +1,30 @@
+import L from 'leaflet';
+import ReactDOMServer from 'react-dom/server';
+import { faTicket } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+// Eventikon
+export const eventIcon = L.divIcon({
+    className: 'custom-marker',
+    html: ReactDOMServer.renderToString(
+      <div style={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: '30px',
+        height: '30px',
+        background: 'linear-gradient(135deg, #ff7eb3, #ffcc00)',
+        borderRadius: '50%',
+        border: '2px solid #d48f00',
+        boxShadow: '0px 2px 5px rgba(0,0,0,0.2)',
+      }}>
+        <FontAwesomeIcon 
+          icon={faTicket} 
+          color="#FFF"  
+          style={{ fontSize: '16px', textShadow: '1px 1px 2px rgba(0,0,0,0.3)' }} 
+        />
+      </div>
+    ),
+    iconSize: [40, 40],
+    iconAnchor: [15, 18],
+  });
