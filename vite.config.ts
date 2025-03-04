50b18a381ce29d8281fd99451aa0d7b0f0f0256c
Tobias Andersson <tobiaserikandersson@gmail.com>
2025-03-04 12:07:54 +0200

    fixed tsconfog

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import tailwindcss from 'tailwindcss';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/reseplanerare-frontend',
   css: {
     postcss: {
       plugins: [tailwindcss()],
