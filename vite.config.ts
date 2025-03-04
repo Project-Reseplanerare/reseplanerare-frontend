24a702fd8571e95896a11cb182c381a0b7f4a70e
Tobias Andersson <tobiaserikandersson@gmail.com>
2025-03-04 11:42:54 +0200

    trigger github pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import tailwindcss from 'tailwindcss';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/reseplanerare-frontend',
   css: {
     postcss: {
       plugins: [tailwindcss()],
